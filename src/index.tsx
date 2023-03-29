81200f1c7d697d2bf70228258ac561cbd230054f
Huy Phung <huypc2410@gmail.com>
2023-03-29 22:20:17 +0700

    [MOD] Fix prettier config

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
-import router from './routes/Router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import router from "./routes/Router";
 
 // import fonts globally
 import "./assets/fonts/inter/Inter-Regular.ttf";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
