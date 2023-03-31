dd9825173a5ab4e5e954bbc7580feaffef8cb281
ThanhHa-Luong <hachan27111992@gmail.com>
2023-03-31 19:39:28 +0700

    implement footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,143 @@
-import { Container } from "@mantine/core";
+import { Container, createStyles, Grid, Group, rem, Text } from "@mantine/core";
 import "./Footer.css";
 import * as React from "react";
+import { myTabs, myMisions, myWorkingHours, myContacts, myFooterContent } from "../../shared";
+import { Link } from "react-router-dom";
+import { TabElement } from "../../shared/types";
+
+const useStyles = createStyles((theme) => ({
+  logo: {
+    color: theme.colors.blue,
+    fontWeight: 800,
+    marginTop: 0,
+    marginBottom: `${rem(16)}`,
+    lineHeight: 1
+  },
+  title: {
+    marginBottom: `${rem(16)}`,
+    fontWeight: 500,
+    lineHeight: 1,
+    color: theme.colors.gray[7],
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+  container: {
+    display: "grid",
+    gridTemplateColumns: "3fr 2fr 3fr 4fr",
+    padding: `${rem(64)} ${rem(24)} ${rem(32)} ${rem(24)}  `,
+    [theme.fn.smallerThan("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+  },
+  groupText: {
+    color: theme.colors.gray[7],
+    fontSize: 16,
+    fontWeight: 400,
+    display: "block",
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+  groupLogo: {
+    color: theme.colors.gray[7],
+    fontSize: 16,
+    fontWeight: 400,
+    display: "block",
+    [theme.fn.smallerThan("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      justifyItems: "center",
+    },
+
+  },
+  groupLink: {
+    display: "block",
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+
+  },
+  link: {
+    display: "block",
+    color: theme.colors.gray[7],
+    fontSize: 16,
+    fontWeight: 400,
+    textDecoration: "none",
+    // lineHeight: 1,
+    '&:hover': {
+      color: theme.colors.blue,
+      fontWeight: 500
+    }
+  },
+  afterFooter: {
+    padding: `${rem(16)} 0 ${rem(64)} 0 `,
+    borderTop: `${rem(1)} solid ${theme.colors.gray[2]}`,
+    justifyContent: "space-between",
+    color: theme.colors.blue[5],
+    fontSize: `${rem(12)}`,
+    [theme.fn.smallerThan("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      justifyItems: "center",
+    },
+  },
+
+}));
+
 
 const Footer = () => {
+  const { classes, cx } = useStyles()
+
+
+  const footerComponents = myFooterContent.map((group) => {
+    var titleStyle = classes.title;
+    var contentStyle = classes.groupText;
+
+    //var contents;
+
+    switch (group.type) {
+      case ("logo"):
+        titleStyle = classes.logo;
+        contentStyle = classes.groupLogo
+        break;
+      case ("tab"):
+        contentStyle = classes.groupLink
+        break;
+      default: break;
+    }
+
+    const contents = group.content.map((el, index) => {
+      if (group.type === "tab")
+      return (
+        <Link to={(el as TabElement).route} className={classes.link}>{(el as TabElement).name}</Link>
+      );
+      return (
+        <Text c="dimmed">{el.toString()}</Text>
+      )
+    })
+
+    return (
+      <Group className={contentStyle}>
+          <Text className={titleStyle}>{group.title}</Text>
+          {contents}
+        </Group>
+    )
+  })
+
+
   return (
     <React.Fragment>
+      <Container size="xl" className={classes.container}>
+        {footerComponents}
+      </Container>
       <Container size="xl">
-        <h1>This is footer</h1>
+        <Group className={classes.afterFooter}>
+          <Text>Copyright © 2020 CÔNG TY SẢN XUẤT EKE MÁY LẠNH THÀNH ĐẠT</Text>
+          <Text>Designed by 200years</Text>
+        </Group>
       </Container>
     </React.Fragment>
   );
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,5 @@
 import AppRoutes from "../routes/Routes";
-import { TabElement } from "./types";
+import { FooterElement, TabElement } from "./types";
 
 export const myTabs: TabElement[] = [
   {
@@ -23,3 +23,45 @@ export const myTabs: TabElement[] = [
     route: AppRoutes.contact,
   },
 ];
+
+export const myMisions : string[] = [
+  "Uy tín - Chất lượng",
+  "Chế độ bảo hành tốt nhất",
+  "Giá cả hợp lý",
+  "Đội ngũ nhân viên chuyên nghiệp",
+];
+
+export const myWorkingHours : string[] = [
+  "Thứ 2 - Thứ 6: 8:00 - 17:00",
+  "Thứ 7: 8:00 - 12:00",
+  "Chủ nhật: Nghỉ",
+];
+
+export const myContacts : string[] = [
+  "0908 779 519",
+  "nguyenhoangkimlong2013@gmail.com",
+  "17/7 Ấp Xuân Thới Đông 1, Xuân Thới Đông. Hóc Môn, Tp Hồ Chí Minh"
+];
+
+export const myFooterContent : FooterElement[] = [
+  {
+    title: "LKV",
+    content: myMisions,
+    type: "logo",
+  },
+  {
+    title: "Đường dẫn",
+    content: myTabs.slice(0, -1),
+    type: "tab",
+  },
+  {
+    title: "Thời gian làm việc",
+    content: myWorkingHours,
+    type: "text",
+  },
+  {
+    title: "Liên hệ",
+    content: myContacts,
+    type: "text",
+  }
+]
